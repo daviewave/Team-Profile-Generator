36baf3f398975c9115d3d906e8bd13b3c627d380
David Silveira <daviewave@gmail.com>
2021-11-18 01:33:06 -0500

    added pseudocode for html-framework.js

---
+++ src/html-framework.js
@@ -0,0 +1,6 @@
+//Create function that generates an html page with cards of each employee added
+//1. create variable function that will eventually generate an exportable html framework
+//2. Loop through each employee and determine their role so the role specific info can be adde
+//3. Create a card for each employee added in the array
+//4. return the html framework
+//5. export the module
