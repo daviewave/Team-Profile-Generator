6f3a10a57d7ea025bc4890371173776ba3e878c5
David Silveira <daviewave@gmail.com>
2021-11-17 23:44:42 -0500

    wrote tests for engineer object instantiations

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,4 +1,29 @@
 //creating test
 //1. import necessities
+const Engineer = require("../lib/Engineer");
+
 //2. create a 'dumby' object to test functions
+const engineerTest = new Engineer(
+  "David",
+  3,
+  "daviewave@gmail.com",
+  "daviewave"
+);
+
 //3. write test functions with asserted values
+describe("Employee", () => {
+  it("HAS THE CORRECT NAME: DAVID", () => {
+    expect(engineerTest.name).toEqual("David");
+  });
+
+  it("HAS THE CORRECT ID: 3", () => {
+    expect(engineerTest.id).toEqual(3);
+  });
+
+  it("HAS THE CORRECT EMAIL ADDRESS: daviewave@gmail.com", () => {
+    expect(engineerTest.email).toEqual("daviewave@gmail.com");
+  });
+  it("HAS THE CORRECT GITHUB USERNAME: daviewave", () => {
+    expect(engineerTest.github).toEqual("daviewave");
+  });
+});
