12324431dac96523270cb79a5f33d5c92eea42e1
David Silveira <daviewave@gmail.com>
2021-11-17 22:02:27 -0500

    added an array of prompts that will be called with inquirer to prompt the user questions about the new employee they would like to add

--- index.js
+++ index.js
@@ -5,3 +5,58 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+
+//DATA
+const allEmployees = []; //array to store user input
+const employeePrompts = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter New Employee's Name: ",
+        validate: function (newName) {
+            if (newName) {
+                return true;
+            } else {
+                return 'ERROR: ENTER NEW EMPLOYEES NAME';
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Enter New Employee's ID: ",
+        validate: function (newID) {
+            if (newID) {
+                return true;
+            } else {
+                return 'ERROR: ENTER NEW EMPLOYEES ID';
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter New Employee's Email Address: ",
+        validate: function (newEmail) {
+            if (newEmail) {
+                return true;
+            } else {
+                return 'ERROR: ENTER NEW EMPLOYEES ID';
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "Enter New Employee's Role at the Company: ",
+        choices: ["Engineer", "Intern", "Manager"]
+    },
+]
+
+//FUNCTIONS
+function enterNewEmployee(){
+    inquirer.prompt(employeePrompts);
+}
+
+enterNewEmployee();
