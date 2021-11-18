3c9eefee652431ab819df80b310d1640bc0d2b8f
David Silveira <daviewave@gmail.com>
2021-11-18 02:09:03 -0500

    updated html-generator.js by adding a funciton to writeToFile()

--- html-generator.js
+++ html-generator.js
@@ -1,7 +1,13 @@
 const fs = require("fs");
 
-function writeToFile(html) {}
+function writeToFile(html) {
+  fs.writeFile("./newHTML/index.html", html, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("SUCCESS: INDEX.HTML PAGE GENERATED");
+    }
+  });
+}
 
-const applyCSS = () => {};
-
-(module.exports = writeToFile), applyCSS;
+module.exports = writeToFile;
